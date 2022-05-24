4c7f0e6b40657681637f41a994edfafae82e9d9c
Elvis Alfaro <ealfaro810@gmail.com>
2022-05-23 18:38:29 -0600

    Creacion de mi primer POST

--- src/controllers/controladorInicio.js
+++ src/controllers/controladorInicio.js
@@ -13,6 +13,19 @@ exports.Inicio = (req, res) => {
     };
     res.json(msj);
 };
-exports.Otra = (req, res) => {
-    res.send("Otra ruta");
+exports.ejemploPost = (req, res) => {
+    //console.log(req.body);
+    const { usuario, contraseña } = req.body;
+    console.log(usuario);
+    console.log(contraseña);
+    const msj = { 
+        mensaje: 'Ninguno'
+    };
+    if(!usuario || !contraseña) {
+        msj.mensaje = 'Debe de enviar los datos completos';
+    }
+    else{
+        msj.mensaje = 'Peticion procesada correctamente';
+    }
+    res.json(msj);
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,5 +2,5 @@ const { Router } = require('express');
 const controladorInicio = require('../controllers/controladorInicio');
 const rutas = Router();
 rutas.get('/', controladorInicio.Inicio);
-rutas.get('/otra', controladorInicio.Otra);
+rutas.post('/post/', controladorInicio.ejemploPost)
 module.exports = rutas;
