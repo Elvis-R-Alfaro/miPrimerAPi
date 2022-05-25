6a5e02a464849520e08ecbb9a856907cc0b5939d
Elvis Alfaro <ealfaro810@gmail.com>
2022-05-24 18:58:44 -0600

    "Creacion de las validacion de vacios y longitud para usuarios y contraseña utilizando express-validation, se creo el modulo delete que recibe unicamente la id y valida que sea entera y no este vacia"

--- src/controllers/controladorInicio.js
+++ src/controllers/controladorInicio.js
@@ -1,3 +1,4 @@
+const { validationResult } = require('express-validator');
 exports.Inicio = (req, res) => {
     const listaModulos = [
         {modulo: "empleados", ruta: "/api/empleados"},
@@ -16,6 +17,8 @@ exports.Inicio = (req, res) => {
 exports.ejemploPost = (req, res) => {
     //console.log(req.body);
     //const { usuario, contraseña } = req.body;
+    const validaciones = validationResult(req);
+    console.log(validaciones.errors);
     const usuario2 = req.body.usuario;
     const contraseña2 = req.body.contraseña;
     console.log(usuario2);
@@ -32,7 +35,9 @@ exports.ejemploPost = (req, res) => {
     res.json(msj);
 };
 exports.ejemploPut = (req, res) => {
-    console.log(req);
+    const validaciones = validationResult(req);
+    console.log(validaciones.errors);
+    //console.log(req);
     const { id } = req.query.id;
     console.log(id);
     //const { usuario, contraseña } = req.body;
@@ -44,6 +49,25 @@ exports.ejemploPut = (req, res) => {
         mensaje: 'Ninguno'
     };
     if(!usuario2 || !contraseña2) {
+        msj.mensaje = 'Debe de enviar los datos completos';
+    }
+    else{
+        msj.mensaje = 'Peticion procesada correctamente';
+    }
+    res.json(msj);
+};
+
+exports.ejemploDelete = (req, res) => {
+    const validaciones = validationResult(req);
+    console.log(validaciones.errors);
+    //console.log(req);
+    const { id } = req.query.id;
+    console.log(id);
+    //const { usuario, contraseña } = req.body;
+    const msj = { 
+        mensaje: 'Ninguno'
+    };
+    if(!id) {
         msj.mensaje = 'Debe de enviar los datos completos';
     }
     else{
