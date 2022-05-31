1f036133ebe47f45275805376052e1b0433cd58b
Elvis Alfaro <ealfaro810@gmail.com>
2022-05-31 10:31:16 -0600

    Creacion del modelo Cargos y la conexion a la base de datos

---
+++ src/configurations/db.js
@@ -0,0 +1,11 @@
+const sequelize = require('sequelize');
+const db = new sequelize(
+    'base de datos',
+    'usuario',
+    'contraseña',
+    {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306,
+    }
+);
