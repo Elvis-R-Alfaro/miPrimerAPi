e7151e8e8fb8818510a9463e0180df87161721a5
Elvis Alfaro <ealfaro810@gmail.com>
2022-05-31 16:14:41 -0600

    Creacion del metodo guardar cargos
    Aun no funciona, pero es comprobable mediante thundercat

--- src/controllers/controladorCargos.js
+++ src/controllers/controladorCargos.js
@@ -10,5 +10,73 @@ exports.Listar = async (req, res) => { //async es para que espere a que se ejecu
         console.error(error);
         res.json(error);
     }
-    //res.json(msj);
+};
+exports.Guardar = async (req, res) => {
+    const validaciones = validationResult(req);
+    console.log(validaciones.errors);
+    const msj = { 
+        mensaje: ''
+    };
+    if(validaciones.errors.length > 0){
+        validaciones.errors.forEach(element => {
+            msj.mensaje += element.msg + '. ';
+        });
+    }
+    else{
+        const { nombre, descripcion } = req.body;
+        try {
+            await modeloCargo.create({
+                nombre: nombre,
+                descripcion: descripcion,
+            });
+            msj.mensaje='Registro almacenado';
+        } 
+        catch (error) {
+            msj.mensaje='Error al guardar los datos';
+        }
+            
+    }
+    res.json(msj);    
+};
+    
+exports.ejemploPut = (req, res) => {
+    const validaciones = validationResult(req);
+    console.log(validaciones.errors);
+    //console.log(req);
+    const { id } = req.query.id;
+    console.log(id);
+    //const { usuario, contraseña } = req.body;
+    const usuario2 = req.body.usuario;
+    const contraseña2 = req.body.contraseña;
+    console.log(usuario2);
+    console.log(contraseña2);
+    const msj = { 
+        mensaje: 'Ninguno'
+    };
+    if(!usuario2 || !contraseña2) {
+        msj.mensaje = 'Debe de enviar los datos completos';
+    }
+    else{
+        msj.mensaje = 'Peticion procesada correctamente';
+    }
+    res.json(msj);
+};
+
+exports.ejemploDelete = (req, res) => {
+    const validaciones = validationResult(req);
+    console.log(validaciones.errors);
+    //console.log(req);
+    const { id } = req.query.id;
+    console.log(id);
+    //const { usuario, contraseña } = req.body;
+    const msj = { 
+        mensaje: 'Ninguno'
+    };
+    if(!id) {
+        msj.mensaje = 'Debe de enviar los datos completos';
+    }
+    else{
+        msj.mensaje = 'Peticion procesada correctamente';
+    }
+    res.json(msj);
 };
--- src/routes/rutasCargos.js
+++ src/routes/rutasCargos.js
@@ -2,21 +2,17 @@ const { Router } = require('express');
 const { body,query } = require('express-validator');
 const controladorCargos = require('../controllers/controladorCargos');
 const rutas = Router();
-rutas.get('/Listar', controladorCargos.Listar);
+rutas.get('/listar', controladorCargos.Listar);
+
+
+rutas.post('/guardar', 
+body('nombre')
+.notEmpty().withMessage('No se aceptan valores vacios para el nombre del cargo')
+.isLength({min: 3}).withMessage('La cantidad minima de caracteres son 3, para el nombre del cargo'),
+controladorCargos.Guardar);
+
 
 /*
-rutas.post('/', 
-body('usuario')
-.notEmpty().withMessage('No se aceptan valores vacios para el usuario')
-.isLength({min: 3}).withMessage('La cantidad minima de caracteres son 3, para el nombre del usuario'),
-body('contraseña')
-.notEmpty().withMessage('No se aceptan valores vacios para la contraseña')
-.isLength({min: 6}).withMessage('La cantidad minima de caracteres son 6, para el la contraseña'),
-
-controladorInicio.ejemploPost);
-
-
-
 rutas.put('/',
 query('id')
 .notEmpty().withMessage('No se aceptan valores vacios para el id del usuario')
